91968a4e4b5a71389ea3389d8495abadd2664bd4
Dmytro Fomin <fodi85999@gmail.com>
2026-04-18 01:34:49 +0200

    Initial Commit

---
+++ ChefOS/ChefOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefOSApp.swift
+//  ChefOS
+//
+//  Created by Дмитрий Фомин on 18/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChefOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefOSTests/ChefOSTests.swift
@@ -0,0 +1,19 @@
+//
+//  ChefOSTests.swift
+//  ChefOSTests
+//
+//  Created by Дмитрий Фомин on 18/04/2026.
+//
+
+import Testing
+@testable import ChefOS
+
+struct ChefOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
